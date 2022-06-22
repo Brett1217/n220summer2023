76ece2955b2a40219d36b25e8eee29d165938655
Brett1217 <Beasleybrett682@gmail.com>
2022-06-21 22:47:57 -0400

    Update app.js

--- Labs/Lab6/ex1/js/app.js
+++ Labs/Lab6/ex1/js/app.js
@@ -1,54 +1,26 @@
-<!DOCTYPE html>
+arr = []
 
-<html lang="en">
-<head>
 
-<!-- <title> tag is used to place the title for web page -->
 
-<title>Welcome user</title>
+function setup() {
+  createCanvas(400, 400);
 
-<meta charset="UTF-8">
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-</head>
-
-<body>
-
-Enter Name :
-
-<!-- textbox for name -->
-
-<input type="text" id="txtName"/>
-
-<br> <br>
-
-<!-- button -->
-
-<input type="button" value="Submit" onclick="welcomeUser()"/>
-
-<!-- <script> tag is used for javascript -->
-
-<script>
-
-//function
-
-function welcomeUser()
-
-{
-
-//getting name entered by user
-
-var userName=document.getElementById("txtName").value;
-
-//display greet message to the user
-
-console.log("Hello "+userName);
-
+  for (let i = 0; i < int(random(15, 20)); ++i) {
+    obj = {x:random(width), y: random(height), size: random(10, 20)};
+    
+    arr.push(obj);
+  }
 }
 
-</script>
-
-</body>
-
-</html>
+function draw() {
+  background(220);
+  
+  arr.forEach(e => {
+    e.x += 1;
+    if (e.x > width) {
+      e.x = 0;
+    }
+    circle(e.x, e.y, e.size);
+  });
+}
+/*********** Output *************/
