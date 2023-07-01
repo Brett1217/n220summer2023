2e9688a2d5a7de2762ba704d9db0999a63760191
Brett1217 <Beasleybrett682@gmail.com>
2023-06-30 22:51:18 -0400

    change

--- Labs/Lab5/ex2/js/app.js
+++ Labs/Lab5/ex2/js/app.js
@@ -1,39 +1,18 @@
-var content = document.getElementById("content");
-var btn = document.getElementById("btn");
-var width = 400;
-btn.addEventListener("click", function() {
-    width +=100;
-    content.style.width = width + "px";
-});
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
+    <style>
+    #mydiv {
+            background-color: green;
+            width: 100px;
+            height: 100px;
+        }
+    </style>
 
-#content {
-    width: 100px;
-    height: 50px;
-    background: lightblue;
-    transition: all 1s ease-in-out
-}
-
-#btn {
-    width: 100px;
-    height: 50px;
-    background: grey;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: white;
-    text-decoration: none;
-}
-
-a {
-    color: white;
-    text-decoration: none;
-  }
-
-
-
-  <><div id="content"></div><div id="btn"><a href="#">Click</a></div></>
-
-
-
-}
+    </head>
+</html>
