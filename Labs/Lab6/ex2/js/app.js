dc54e88ca9725102ff558005cc10a3487343c586
Brett1217 <Beasleybrett682@gmail.com>
2023-07-18 14:58:19 -0400

    Update app.js

--- Labs/Lab6/ex2/js/app.js
+++ Labs/Lab6/ex2/js/app.js
@@ -1,36 +1,23 @@
-(function () {
-  // global variables that will be loaded/initialized later
-  let canvas, ctx, gravity, ball, friction
-  // runs once at the beginning
-  // loads any data and kickstarts the loop
-  function init () {
-  // load data here
-  canvas = document.getElementById('gameCanvas')
-  ctx = canvas.getContext('2d')
-  // set the canvas size
-  canvas.width = 800
-  canvas.height = 800
-  // world/scene settings
-  gravity = 0.25
-  friction = 0.98
-  // begin update loop
-  window.requestAnimationFrame(update)
-  }
-  // draws stuff to the screen
-  // allows us to separate calculations and drawing
-  function draw () {
-  // clear the canvas and redraw everything
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  }
-  // the main piece of the loop
-  // runs everything
-  function update () {
-  // queue the next update
-  window.requestAnimationFrame(update)
-  // logic goes here
-  // draw after logic/calculations
-  draw()
-  }
-  // start our code once the page has loaded
-  document.addEventListener('DOMContentLoaded', init)
-  })()
+let graphic;
+
+function setup() {
+  createCanvas(400, 400);
+  
+  // Initialize the graphic object
+  graphic = {
+    width: 100,
+    height: 100,
+    color: 'red',
+    x: 200,
+    y: 200
+  };
+}
+
+function draw() {
+  background(220);
+  
+  // Draw the graphic using the object's properties
+  fill(graphic.color);
+  rect(graphic.x, graphic.y, graphic.width, graphic.height);
+}
+
